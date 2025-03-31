5ea0d4780ac431929799167b7490db5c78788ad2
Sarthak <sarthak1662.becse24@chitkara.edu.in>
2025-03-31 22:07:25 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,13 +4,12 @@ int fibonacciSeries(n){
     int a =0;
     int b=1;
     int sum=0;
-    for(int i=1;i<n;i++){
+    for(int i=1;i<n-2;i++){
         sum=a+b;
         a=b;
         b=sum;
     }
     return sum;
-    return ;
 }
 int main(){
     int n;

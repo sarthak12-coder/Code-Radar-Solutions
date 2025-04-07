f2efcdcefe86b7441f9b4672be02470d6985bf3f
Sarthak <sarthak1662.becse24@chitkara.edu.in>
2025-04-07 20:47:00 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
-void Sequence(int *arr, int n){
-    int length =0;
-    for(int i=0 i<n;i++){
-        if(arr[i]<arr[i+1]){
-            length+=1;
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int longestConsecutive(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
+
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int longest = 1;
+    int currentStreak = 1;
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] == nums[i - 1]) continue;
+
+        if (nums[i] == nums[i - 1] + 1) {
+            currentStreak++;
+        } else {
+            if (currentStreak > longest) {
+                longest = currentStreak;
+            }
+            currentStreak = 1;
         }
     }
-    printf("%d",length);
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for (int i=0; i<n;i++){
-        scanf("%d",&arr[i]);
+
+    if (currentStreak > longest) {
+        longest = currentStreak;
     }
-    Sequence(arr,n);
-    return 0;  
+
+    return longest;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int result = longestConsecutive(arr, n);
+    printf("%d\n", result);
+
+    return 0;
 }
